567a440e89f74d3b3ab81afb5b53ea0c9c6ef32c
Alan Miranda <ti040@irmaosgoncalves.com.br>
2021-11-04 20:14:41 -0400

    Adiciona ContactsController

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
 
 import customers from "./app/controllers/CustomersController";
+import contacts from "./app/controllers/ContactsController";
 
 const routes = new Router();
 
+// customers
 routes.get("/customers", customers.index);
 routes.get("/customers/:id", customers.show);
 routes.post("/customers", customers.create);
 routes.put("/customers/:id", customers.update);
 routes.delete("/customers/:id", customers.destroy);
+
+// customers/1/contacts
+routes.get("/customers/:customerId/contacts", contacts.index);
+routes.get("/customers/:customerId/contacts/:id", contacts.show);
+routes.post("/customers/:customerId/contacts", contacts.create);
+routes.put("/customers/:customerId/contacts/:id", contacts.update);
+routes.delete("/customers/:customerId/contacts/:id", contacts.destroy);
 
 export default routes;
